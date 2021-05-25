28f8b4d78400c2d88ed556e6d473c40fff3f8d8b
Exortions <75327059+Exortions@users.noreply.github.com>
2021-05-25 12:44:23 -0700

    Implement MessageConfig for Wipe

--- src/main/java/com/itech4kids/skyblock/Commands/Moderation/WipeCommand.java
+++ src/main/java/com/itech4kids/skyblock/Commands/Moderation/WipeCommand.java
@@ -12,20 +12,20 @@ public class WipeCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         if (args.length == 0){
-            sender.sendMessage(ChatColor.RED + "Specify the player please!");
+            sender.sendMessage(MessageConfig.specifyPlayer());
         }else{
             try {
                 Player cheater = Bukkit.getPlayer(args[0]);
-                cheater.sendMessage(ChatColor.RED + "Cheating has been detected on one or more of your Skyblock Profiles and your profiles have been wiped.");
+                cheater.sendMessage(MessageConfig.wipeMsg());
                 cheater.performCommand("warp hub");
                 IslandManager.deleteWorld(cheater);
                 if (sender instanceof Player){
-                    ((Player) sender).performCommand("ban " + cheater.getName() + " Boosting detected on multiple Skyblock profiles");
+                    ((Player) sender).performCommand("ban " + cheater.getName() + " Boosting_detected_on_multiple_Skyblock_profiles_and_your_profiles_have_been_wiped.");
                 }else{
                     cheater.kickPlayer(ChatColor.RED + "Boosting detected on multiple Skyblock profiles");
                 }
             }catch (Exception e){
-                sender.sendMessage(ChatColor.RED + "This player is not onlinr!");
+                sender.sendMessage(MessageConfig.notOnline());
             }
         }
 
